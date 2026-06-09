49e50d73bb64568afa68816afb01ed513cfbf540
Juggernaut <rein@catatonic.ai>
2026-06-09 11:47:05 +0300

    save

--- Home/homepage.py
+++ Home/homepage.py
@@ -1,13 +1,22 @@
 """Homepage for the Home remote."""
 
+import atlantis
+
 
 @homepage
 async def homepage() -> dict:
     """Return an empty homepage and startup commands."""
 
+    app_path = atlantis.get_script_folder()
+    app_commands = [
+        f"/cd {app_path}",
+        f"/path unshift {app_path}",
+    ] if app_path else []
+
     return {
         "html": "<div></div>",
         "commands": [
+            *app_commands,
             "/terminal on",
             "/app on",
             "/callback set chat auto"
