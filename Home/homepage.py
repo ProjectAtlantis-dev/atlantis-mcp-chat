f2e63da4f46ac9ca562a5c2837a21da224baa826
Juggernaut <rein@catatonic.ai>
2026-06-16 13:28:23 +0300

    save

--- Home/homepage.py
+++ Home/homepage.py
@@ -1,12 +1,13 @@
 """Homepage for the Home remote."""
 
+from pathlib import Path
 from typing import Optional
 
 import atlantis
 
 
 @public
-async def home_menu() -> Optional[str]:
+async def home_menu():
     """Let the user choose where to go next."""
     from .modal import modal_menu
 
@@ -22,14 +23,26 @@ async def home_menu() -> Optional[str]:
     if choice is None:
         return None
 
+    script_folder = atlantis.get_script_folder()
+
     choice_id = str(choice["id"])
     if choice_id == "explore_demo_folder":
         commands = [
+            f"/cd {script_folder}",
             "cd ../..",
             "cd Demo"
         ]
         await atlantis.client_command("/script", {"commands":commands})
-    return choice_id
+
+        img_path = Path(__file__).absolute().parents[4] / "sitting_coffee.png"
+        await atlantis.client_image(
+            str(img_path),
+            content="Demo folder coming right up...",
+            max_width="25vw",
+        )
+
+        await atlantis.client_command("/script", {"commands":["ls"]})
+
 
 
 @public
